6624e1d57367af29fb45e19521bfa1e2be3f0495
lmalhaim <63025546+lmalhaim@users.noreply.github.com>
2021-11-08 19:30:47 -0600

    Done// navbar toggle issue

--- Client/scripts/index.js
+++ Client/scripts/index.js
@@ -1,7 +1,18 @@
-const swup = new Swup()
+let collapseClick = false
+/*
+function navCollapseStyle(){
+    if(!collapseClick){
+        document.querySelector('#topBar').style.height = '250px'
+    }
+    else{
+        document.querySelector('#topBar').style.height = '80px'
+    }
+    collapseClick = !collapseClick
+}*/
 
-function navClick(tab){
-    navigation = document.querySelector('#nav_animation')
-    navigation.className += ` ${tab}_selected`
-    
-}
+navLinks.forEach((l) => {
+    const navLinks = document.querySelectorAll('.nav-link')
+    const menuToggle = document.getElementById('toggleMobileMenu')
+    const bsCollapse = new bootstrap.Collapse(menuToggle)
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
